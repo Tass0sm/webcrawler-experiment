fa3de73622396f14f25a1d4e9ebc4efe7a636fef
Tassos Manganaris <tassos.manganaris@gmail.com>
2020-12-01 19:40:44 -0500

    Printing Tags. Removed build files.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
 import Text.HTML.TagSoup
+import System.Environment
+
+printLinks :: String -> IO ()
+printLinks file = do
+  tags <- parseTags <$> readFile file
+  let strings = map show tags
+  mconcat $ putStrLn <$> strings 
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  rootFile <- head <$> getArgs
+  printLinks rootFile
+  putStrLn rootFile
